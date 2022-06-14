e4ee5a3289526712acc1af20cb62fd124b4fc66e
Alejandro Zuluaga Baron <alejo.zuluagab@gmail.com>
2022-06-14 15:20:50 -0500

    Some changes

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,21 +15,21 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
-        <meta name="author" content="craftzdog" />
+        <meta name="description" content="Alejandro's homepage" />
+        <meta name="author" content="Alejandro Zuluaga" />
+        <meta name="author" content="zuluagab99" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="Alejandro Zuluaga" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta name="twitter:site" content="zuluagab99" />
+        <meta name="twitter:creator" content="zuluagab99" />
+        {/* <meta name="twitter:image" content="https://www.craftz.dog/card.png" /> */}
+        <meta property="og:site_name" content="Alejandro Zuluaga" />
+        <meta name="og:title" content="Alejandro Zuluaga" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://www.craftz.dog/card.png" />
-        <title>Takuya Matsuyama - Homepage</title>
+        <title>Alejandro Zuluaga - homepage</title>
       </Head>
 
       <NavBar path={router.asPath} />
